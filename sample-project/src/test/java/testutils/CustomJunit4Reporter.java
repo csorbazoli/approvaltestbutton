52ef5aa28f780bfc9b7e736b98d2721c5478fce5
csorbazoli <csorbazoli@gmail.com>
2026-03-03 11:44:43 +0000

    refactor CustomJunit4Reporter as enum

--- sample-project/src/test/java/testutils/CustomJunit4Reporter.java
+++ sample-project/src/test/java/testutils/CustomJunit4Reporter.java
@@ -3,20 +3,22 @@ package testutils;
 import java.io.File;
 
 import org.approvaltests.core.ApprovalFailureReporter;
+import org.junit.Assert;
 
 import com.spun.util.io.FileUtils;
 
-public class CustomJunit4Reporter implements ApprovalFailureReporter {
+public enum CustomJunit4Reporter implements ApprovalFailureReporter {
 
-	@Override
-	public boolean report(String received, String approved) {
-		String actual = FileUtils.readFile(received);
-		File approvedFile = new File(approved);
-		String expected = approvedFile.canRead() ? FileUtils.readFile(approved) : "";
-		if (!actual.equals(expected)) {
-			throw new TestResourceComparisonFailure(approvedFile, expected, actual);
-		}
-		return true;
-	}
+    INSTANCE;
+
+    @Override
+    public boolean report(String received, String approved) {
+        String actual = FileUtils.readFile(received);
+        File approvedFile = new File(approved);
+        String expected = approvedFile.canRead() ? FileUtils.readFile(approved) : "";
+        Assert.assertEquals("Actual result does not match content of " + approvedFile.getAbsolutePath(), expected,
+                actual);
+        return true;
+    }
 
 }
